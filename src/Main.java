a277bb703b3a77a66d02604623f5a8bdd4d682b4
George Fasidis <dai16020@uom.edu.gr>
2017-05-31 18:33:19 +0300

    UoMslack servers Added comments

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,18 @@ public class Main {
 		
 		//UoMslack boot screen
 		SplashScreen.startUOMslack();
+		
+		/*
+		 * PROSOXH!
+		 * Se periptwsh poy sthn Login Page den emfanizontai swsta ta ellinika 
+		 * uparxei arxeio me ton kwdika pou apeteitai.
+		 * Onoma arxeiou: Code
+		 * H allagh prepei na ginei stis grammes 65 ews 81 
+		 * kathws kai stis grammes 186 ews 202 ths klashs LoginPage
+		 */
+		
+		/*
+		 * Gia na trexei to programma xreiazontai oi servers pou uparxoun sto zip arxeio poy didetai
+		 */
 	}
 }
